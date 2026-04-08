ed6f6b6b5323e7a25433b329a8a7e31324e8ff6b
Shadow Pro <hello@vetsinpocket.com>
2026-04-08 15:46:00 +0800

    Force dynamic admin and tracking pages

--- app/order/[id]/page.js
+++ app/order/[id]/page.js
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import OrderTrackingClient from '@/components/OrderTrackingClient'
 
 export default async function OrderTrackingPage({ params }) {
